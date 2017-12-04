d66535a971e010553da052d05b53262c03ea820a
Daniel Suskevic <dsuskevic@klt-applemacmookpro1.local>
2017-12-04 21:51:27 +0200

    Corrected constarins

---
+++ LogicalCalculator/LogicalCalculator-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  LogicalCalculator-Bridging-Header.h
+//  LogicalCalculator
+//
+//  Created by Daniel Suskevic on 04/12/2017.
+//  Copyright © 2017 Daniel Suskevic. All rights reserved.
+//
+
+#ifndef LogicalCalculator_Bridging_Header_h
+#define LogicalCalculator_Bridging_Header_h
+
+#import <sqlite3.h>
+
+#endif /* LogicalCalculator_Bridging_Header_h */
